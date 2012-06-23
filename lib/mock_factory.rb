8ba906d838169714b616132107ca80f8840b6f4f
Dean Brundage <github@deanandadie.net>
2012-06-23 10:00:50 -0500

    Need to call `setup`

--- lib/mock_factory.rb
+++ lib/mock_factory.rb
@@ -5,19 +5,25 @@ require 'rspec/rails/mocks'
 
 class MockFactory
 
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
 
   class << self
     include RSpec::Rails::Mocks
-    include RSpec::Mocks::ExampleMethods
 
     def create(const)
       name = const.is_a?(String) ? const : const.name
-      @mocks ||= {}
+      setup
       return @mocks[name] if @mocks[name]
       @mocks[name] = mock_model name
       yield @mocks[name] if block_given?
       @mocks[name]
+    end
+
+  protected
+
+    def setup
+      @mocks ||= {}
+      @space ||= RSpec::Mocks::setup(self)
     end
 
   end
