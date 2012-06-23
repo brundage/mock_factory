62274850a916623cf259bd0dcbacdab09c9fafb8
Dean Brundage <github@deanandadie.net>
2012-06-23 10:27:47 -0500

    differentiating between create & fetch

--- lib/mock_factory.rb
+++ lib/mock_factory.rb
@@ -1,30 +1,45 @@
-require 'rails'
 require 'rspec/core'
 require 'rspec/mocks'
 require 'rspec/rails/mocks'
 
 class MockFactory
 
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 
   class << self
     include RSpec::Rails::Mocks
 
-    def create(const)
-      name = const.is_a?(String) ? const : const.name
-      setup
-      return @mocks[name] if @mocks[name]
-      @mocks[name] = mock_model name
-      yield @mocks[name] if block_given?
-      @mocks[name]
+    def create(const, &block)
+      mock_it 'create', const, &block
     end
 
-  protected
+
+    def fetch(const, &block)
+      mock_it 'fetch', const, &block
+    end
+
+  private
+
+    def mock_it(action, const, &block)
+      setup
+      name = const.is_a?(String) ? const : const.name
+
+      mock_object = if action == 'fetch'
+                      @mocks[name] ||= mock_model name
+                    else
+                      mock_model name
+                    end
+
+      block.call(mock_object) if block
+      mock_object
+    end
+
 
     def setup
       @mocks ||= {}
       @space ||= RSpec::Mocks::setup(self)
     end
+
 
   end
 
--- spec/mock_factory_spec.rb
+++ spec/mock_factory_spec.rb
@@ -3,14 +3,13 @@ require 'mock_factory'
 
 describe MockFactory do
 
-  it "makes a mock object" do
-    defined?(Blark).should be_nil
+  it "create makes a mock object" do
     o = MockFactory.create 'Blark'
     o.should be_a(Blark)
   end
 
 
-  it "applies a block to the mock" do
+  it "create applies a block to the mock" do
     b = MockFactory.create 'Badger' do |b|
           b.stub(:honey).and_return(true)
         end
@@ -18,9 +17,30 @@ describe MockFactory do
   end
 
 
-  it "makes the same object" do
+  it "create makes a different object" do
     stout = MockFactory.create 'Beer'
     ipa = MockFactory.create 'Beer'
+    stout.should_not === ipa
+  end
+
+
+  it "fetch makes a mock object" do
+    o = MockFactory.fetch 'Blark'
+    o.should be_a(Blark)
+  end
+
+
+  it "fetch applies a block to the mock" do
+    b = MockFactory.fetch 'Badger' do |b|
+          b.stub(:honey).and_return(true)
+        end
+    b.honey.should be_true
+  end
+
+
+  it "fetch makes the same object" do
+    stout = MockFactory.fetch 'Beer'
+    ipa = MockFactory.fetch 'Beer'
     stout.should === ipa
   end
 
